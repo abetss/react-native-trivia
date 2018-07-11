505f5f54012c1e2631108461a9303acfb553ac04
Abtin Ghods <abtin.ghods@gmail.com>
2018-07-11 14:59:42 -0400

    Adds loader to quiz screen

--- src/core/components/primitive-components/index.ts
+++ src/core/components/primitive-components/index.ts
@@ -1,3 +1,4 @@
 export * from './base-components';
 export * from './container.component';
 export * from './card.component';
+export * from './loader.component';
--- src/features/trivia/screens/trivia-quiz/trivia.quiz.component.tsx
+++ src/features/trivia/screens/trivia-quiz/trivia.quiz.component.tsx
@@ -8,6 +8,7 @@ import {
   Card,
   Box,
   Button,
+  ActivityIndicator,
 } from 'src/core/components';
 import { getThemeFontSize } from 'src/design-system';
 
@@ -30,48 +31,47 @@ export const TriviaQuizComponent: React.SFC<TriviaQuizProps> = ({
   handleAnswerClick,
 }) => (
   <Container>
-    {isLoading && (
-      <Text>Is Loading</Text>
-    ) /* Todo: switch this with a proper loading */}
-    {!isLoading && (
-      <React.Fragment>
-        <Header>
-          <Text
-            fontSize={4}
-            fontWeight="bold"
-            textAlign="center"
-            color="white.0"
-          >
-            {currentQuestionCategory}
-          </Text>
-        </Header>
-        <Box px={2} flex={1} alignItems="center" pt={5}>
-          <Card height={260} width={320}>
-            <Text textAlign="center">{currentQuestionQuestion}</Text>
-          </Card>
-          <Box pt={3}>
-            <Text>
-              {currentQuestionNumber} of {QUIZ_TOTAL_QUESTIONS}
-            </Text>
+    <React.Fragment>
+      <Header>
+        <Text fontSize={4} fontWeight="bold" textAlign="center" color="white.0">
+          {currentQuestionCategory}
+        </Text>
+      </Header>
+      <Box px={2} flex={1} alignItems="center" pt={5}>
+        {isLoading && (
+          <Box flex={1} justifyContent="center">
+            <ActivityIndicator size="large" />
           </Box>
-          <Box flexDirection="row" pt={5}>
-            <Button
-              title="In Correct"
-              onPress={handleAnswerClick(false)}
-              icon={{ name: 'close', size: getThemeFontSize(4) }}
-              bg="orange.0"
-              w={150}
-            />
-            <Button
-              title="Correct"
-              onPress={handleAnswerClick(true)}
-              icon={{ name: 'check', size: getThemeFontSize(4) }}
-              bg="green.0"
-              w={150}
-            />
-          </Box>
-        </Box>
-      </React.Fragment>
-    )}
+        )}
+        {!isLoading && (
+          <React.Fragment>
+            <Card height={260} width={320}>
+              <Text textAlign="center">{currentQuestionQuestion}</Text>
+            </Card>
+            <Box pt={3}>
+              <Text>
+                {currentQuestionNumber} of {QUIZ_TOTAL_QUESTIONS}
+              </Text>
+            </Box>
+            <Box flexDirection="row" pt={5}>
+              <Button
+                title="In Correct"
+                onPress={handleAnswerClick(false)}
+                icon={{ name: 'close', size: getThemeFontSize(4) }}
+                bg="orange.0"
+                w={150}
+              />
+              <Button
+                title="Correct"
+                onPress={handleAnswerClick(true)}
+                icon={{ name: 'check', size: getThemeFontSize(4) }}
+                bg="green.0"
+                w={150}
+              />
+            </Box>
+          </React.Fragment>
+        )}
+      </Box>
+    </React.Fragment>
   </Container>
 );
